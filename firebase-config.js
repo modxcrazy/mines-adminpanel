68fd05aa07a0e8edc1b9d3e63ba5c6dbf88e9f01
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-12 10:31:59 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCn553qWsVz2VF1dZ4Ji5OkQDGFvMORbJE",
+    authDomain: "pg-data-ed1c2.firebaseapp.com",
+    databaseURL: "https://pg-data-ed1c2-default-rtdb.firebaseio.com",
+    projectId: "pg-data-ed1c2",
+    storageBucket: "pg-data-ed1c2.firebasestorage.app",
+    messagingSenderId: "884858492190",
+    appId: "1:884858492190:web:67b7606ff790064ef3250f",
+    measurementId: "G-J73RD8EH7W"
+};
+
+firebase.initializeApp(firebaseConfig);
